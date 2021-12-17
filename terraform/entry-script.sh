0a5810e71b06950975cdb0d1772b91b43645a87d
joeshiett <josepheshiett@gmail.com>
2021-12-17 14:16:27 +0100

    New commit

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install docker.io
-sudo systemctl start docker 
-sudo usermod -aG docker ubuntu
+
+apt-get update && sudo apt-get install docker.io
+systemctl start docker 
+usermod -aG docker ubuntu
 
 # install docker-compose 
-sudo curl -L "https://github.com/docker/compose/releases/download/2.2.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/2.2.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
