04254bc511f0bfaec45467491be7d93d01199d76
joeshiett <josepheshiett@gmail.com>
2021-12-17 13:56:31 +0100

    New commit

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-     focal \
-     stable"
-apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt-get update && sudo apt-get install docker.io
 sudo systemctl start docker 
 sudo usermod -aG docker ubuntu
 
