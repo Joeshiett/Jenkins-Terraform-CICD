1007bf67cb34d8ecf082c219ec271be3a0ab835a
joeshiett <josepheshiett@gmail.com>
2021-12-17 16:07:15 +0100

    New commit

--- server-cmds.sh
+++ server-cmds.sh
@@ -2,6 +2,6 @@
 
 export DOCKER_USER=$1
 export DOCKER_PWD=$2
-echo $DOCKER_PWD | docker login -u $DOCKER_USER --password-stdin
-docker-compose -f docker-compose.yml up --detach
+echo $DOCKER_PWD | sudo docker login -u $DOCKER_USER --password-stdin
+sudo docker-compose -f docker-compose.yml up --detach
 echo "success"
